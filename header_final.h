ab7a63adc4738c2019bb3cab055694400d850081
fabianaluna1920 <47615118+fabianaluna1920@users.noreply.github.com>
2019-05-12 07:53:41 -0500

    Create header_final.h

---
+++ header_final.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_FINAL_H
+#define HEADER_FINAL_H
+
+
+void choque_elastico(int m1, int m2, float vi1, float vi2);
+void rebote(float h, int masa);
+void movimiento_parabolico(int v0, int pos0, int ang, int t);
+void movi_parabolico_friccion(int v0, int pos0, int ang, int t, int rad,int k,int masa);
+
+
+
+#endif // HEADER_FINAL_H
